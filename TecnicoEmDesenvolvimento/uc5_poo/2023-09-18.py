5c5026c4f1979d8ab6798a369b640efacf081537
bergdantas <54481994+bergdantas@users.noreply.github.com>
2023-11-07 09:48:49 -0300

    saída de dados

---
+++ TecnicoEmDesenvolvimento/uc5_poo/2023-09-18.py
@@ -0,0 +1,14 @@
+#imprimindo uma string
+print("E aí, pessoal")
+
+#imprimindo variáveis
+x='Olá'
+y=','
+z='pessoal'
+print(x, y, z)
+
+#imprimindo variáveis com separadores
+print(x, y, z, sep='-')
+
+#imprimindo variáveis com separadores e marcador de fim de linha
+print(x, y, z, sep='-', end='#')
