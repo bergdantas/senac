7ff64d3283eff3fa70ac68d455231eb50e0ede0f
bergdantas <54481994+bergdantas@users.noreply.github.com>
2023-11-10 10:46:18 -0300

    model

--- TecnicoEmDesenvolvimento/uc5_poo/2023-11-09/model/produto_model.py
+++ TecnicoEmDesenvolvimento/uc5_poo/2023-11-09/model/produto_model.py
@@ -1,4 +1,5 @@
 from db import _executar
+
 class Produto:
     def __init__(self, nome, preco, id=None):
         self.nome=nome
@@ -8,15 +9,18 @@ class Produto:
     
         query="CREATE TABLE IF NOT EXISTS produto(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, preco REAL, status NUMERIC)" #se a tabela produtos ainda não existir, crio uma
         _executar(query)
-        
+
+    #método para criar um produtos   
     def salvarProduto(self):
         query=f"INSERT INTO produto(nome, preco, status) VALUES ('{self.nome}', '{float(self.preco)}', '{self.status}')"
         _executar(query)
     
+    #método para atualizar o status do produto
     def atualizarProduto(self):
         query=f"UPDATE produto SET status='{int(self.status)}' WHERE id='{int(self.id)}'"
         _executar(query)
     
+    #método para excluir produtos
     def excluirProduto(self):
         query=f"DELETE FROM produto WHERE id='{self.id}'"
         _executar(query)
@@ -28,7 +32,7 @@ class Produto:
         produtos=_executar(query)
         return produtos
 
-    #método para buscar um produto cadastrados
+    #método para buscar um produto cadastrado
     @staticmethod
     def get_produto(id):
         query=f"SELECT * FROM produto WHERE id={int(id)}"
