d032868e326982804780195e1465bbb89a073a20
bergdantas <54481994+bergdantas@users.noreply.github.com>
2023-11-17 12:08:18 -0300

    ui segunda aplicação

--- TecnicoEmDesenvolvimento/uc5_poo/2023-11-17/ui3/main.py
+++ TecnicoEmDesenvolvimento/uc5_poo/2023-11-17/ui3/main.py
@@ -7,16 +7,16 @@ app = QApplication(sys.argv)
 botao1 = QPushButton('1')
 serifFont = QFont("Times", 20, QFont.Bold)
 botao1.setFont(serifFont)
-botao1.show()
+#botao1.show()
 
 botao2 = QPushButton('2')
 sansFont = QFont("Helvetica", 32)
 botao2.setFont(sansFont)
-botao2.show()
+#botao2.show()
 
 botao3 = QPushButton('3')
 botao3.setStyleSheet('font-size:40px;color:blue')
-botao3.show()
+#botao3.show()
 
 central_widget = QWidget() #classe genérica
 layout = QVBoxLayout() #layout
